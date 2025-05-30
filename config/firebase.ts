9ef9a1f5270070bf1b45718e5ae120dc2c8b0846
Petrus Handika <petrushandikasinaga@gmail.com>
2025-05-30 16:37:13 +0700

    fix: fix credential firebase

--- config/firebase.ts
+++ config/firebase.ts
@@ -9,14 +9,22 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyApAHpPVpfZwvKBKhbyPYIxvJ8guhlQTL8",
-  authDomain: "expense-tracker-be92a.firebaseapp.com",
-  projectId: "expense-tracker-be92a",
-  storageBucket: "expense-tracker-be92a.firebasestorage.app",
-  messagingSenderId: "999457415106",
-  appId: "1:999457415106:web:7d26feb76bc0de7889805b",
-  //   measurementId: "G-YSDST038TP",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
