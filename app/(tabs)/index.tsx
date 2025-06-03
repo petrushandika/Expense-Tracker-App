f8c0b59b0d2290e7d1e948560d2fb42c36e99122
Petrus Handika <petrushandikasinaga@gmail.com>
2025-06-03 15:16:53 +0700

    style: implement home tab component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,29 +2,113 @@ import Button from "@/components/Button";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { auth } from "@/config/firebase";
-import { colors } from "@/constants/theme";
+import { colors, spacingX, spacingY } from "@/constants/theme";
 import { useAuth } from "@/contexts/authContext";
+import { verticalScale } from "@/utils/styling";
 import { signOut } from "firebase/auth";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import * as Icons from "phosphor-react-native";
+import HomeCard from "@/components/HomeCard";
+import TransactionList from "@/components/TransactionList";
+import { useRouter } from "expo-router";
 
 const Home = () => {
   const { user } = useAuth();
-  // console.log("user: ", user);
+  const router = useRouter();
 
-  // const handleLogout = async () => {
-  //   await signOut(auth);
-  // };
   return (
     <ScreenWrapper>
-      <Typo>Home</Typo>
-      {/* <Button onPress={handleLogout}>
-        <Typo color={colors.black}>Logout</Typo>
-      </Button> */}
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={{ gap: 4 }}>
+            <Typo size={16} color={colors.neutral400}>
+              Hello,
+            </Typo>
+            <Typo size={20} fontWeight={"500"}>
+              {user?.name}
+            </Typo>
+          </View>
+
+          <TouchableOpacity style={styles.searchIcon}>
+            <Icons.MagnifyingGlass
+              size={verticalScale(22)}
+              color={colors.neutral200}
+              weight="bold"
+            />
+          </TouchableOpacity>
+        </View>
+        import * as Icons from "phosphor-react-native";
+        <ScrollView
+          contentContainerStyle={styles.scrollViewStyle}
+          showsVerticalScrollIndicator={false}
+        >
+          <View>
+            <HomeCard />
+          </View>
+
+          <TransactionList
+            data={[1, 2, 3]}
+            loading={false}
+            emptyListMessage="No Transactions added yet!"
+            title="Recent Transactions"
+          />
+        </ScrollView>
+        <Button
+          style={styles.floatingButton}
+          onPress={() => router.push("/(modals)/transactionModal")}
+        >
+          <Icons.Plus
+            color={colors.black}
+            weight="bold"
+            size={verticalScale(24)}
+          />
+        </Button>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+    marginTop: verticalScale(8),
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    padding: spacingX._10,
+    borderRadius: 50,
+  },
+
+  floatingButton: {
+    height: verticalScale(50),
+    width: verticalScale(50),
+    borderRadius: 100,
+    position: "absolute",
+    bottom: verticalScale(30),
+    right: verticalScale(30),
+  },
+
+  scrollViewStyle: {
+    marginTop: spacingY._10,
+    paddingBottom: verticalScale(100),
+    gap: spacingY._25,
+  },
+});
